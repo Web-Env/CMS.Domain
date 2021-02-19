f3bc433d1ca95107ba7a0162d2b2d2f197b4a169
AdamBOD <adambarryodonovan@gmail.com>
2021-02-19 00:12:10 +0000

    Setup Testing

---
+++ CMS.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace CMS.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var test = true;
+            Assert.Equal(test, true);
+        }
+    }
+}
