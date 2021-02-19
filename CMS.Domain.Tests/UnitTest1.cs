401ba14d1704f2101e2495983c759448c091852a
AdamBOD <adambarryodonovan@gmail.com>
2021-02-19 00:32:26 +0000

    Update UnitTest1.cs

--- CMS.Domain.Tests/UnitTest1.cs
+++ CMS.Domain.Tests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace CMS.Domain.Tests
         public void Test1()
         {
             var test = true;
-            Assert.Equal(test, true);
+            Assert.True(test);
         }
     }
 }
