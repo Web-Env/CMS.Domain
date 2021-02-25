6e3bfca8280fa38e759a42a18b0bde6c019d9c17
Sean Harrington <seantharri05@gmail.com>
2021-02-25 17:46:16 +0000

    Setup Audit Log Tests

---
+++ Tests/CMS.Domain.Repositories.Tests/AuditLogRepositoryTests.cs
@@ -0,0 +1,16 @@
+﻿using CMS.Domain.Tests.Funcs;
+using CMS.Domain.Tests.Helpers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CMS.Domain.Repositories.Tests
+{
+    [Trait("Category", "Unit")]
+    public class AuditLogRepositoryTests : RepositoryTestBase
+    {
+        public AuditLogRepositoryTests(RepositoryDatabaseFixture fixture) : base(fixture) { } 
+        
+    }
+}
---
+++ Tests/CMS.Domain.Tests/Helpers/AuditLogHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using CMS.Domain.Entities;
+using CMS.Domain.Enums; 
+
+namespace CMS.Domain.Tests.Helpers
+{
+    public static class AuditLogHelper
+    {
+        public static AuditLog CreateOneAuditLogObject(User user) {
+
+            return new AuditLog
+            {
+                Id = Guid.NewGuid(),
+                ActionCategory = (short)UserActionCategory.Entry,
+                Action = (short)UserAction.Create,
+                UserId = user.Id,
+                UserAddress = user.Email,
+                OccurredOn = DateTime.Now
+            };
+        
+        }
+
+    }
+}
