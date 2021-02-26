8fdd29b78ee2eab714efa0b8e2a7096ce43a4e36
Sean Harrington <seantharri05@gmail.com>
2021-02-26 13:25:21 +0000

    Set up User for AuditLog Test

--- Tests/CMS.Domain.Repositories.Tests/AuditLogRepositoryTests.cs
+++ Tests/CMS.Domain.Repositories.Tests/AuditLogRepositoryTests.cs
@@ -1,8 +1,6 @@
-﻿using CMS.Domain.Tests.Funcs;
-using CMS.Domain.Tests.Helpers;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CMS.Domain.Entities;
+using CMS.Domain.Tests.Funcs;
+using Nito.AsyncEx;
 using Xunit;
 
 namespace CMS.Domain.Repositories.Tests
@@ -10,7 +8,14 @@ namespace CMS.Domain.Repositories.Tests
     [Trait("Category", "Unit")]
     public class AuditLogRepositoryTests : RepositoryTestBase
     {
-        public AuditLogRepositoryTests(RepositoryDatabaseFixture fixture) : base(fixture) { } 
+        private readonly User _user;
+
+        public AuditLogRepositoryTests(RepositoryDatabaseFixture fixture) : base(fixture)
+        {
+            _user = AsyncContext.Run(() => UserFunc.CreateOneUser(GetContext()));
+        }
         
+
+
     }
 }
---
+++ Tests/CMS.Domain.Tests/Consts/UserConsts.cs
@@ -0,0 +1,7 @@
+﻿namespace CMS.Domain.Tests.Consts
+{
+    public static class UserConsts
+    {
+        public static string DefaultUserAddress { get; } = "255.255.255.255";
+    }
+}
