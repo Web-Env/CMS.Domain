b8a230abba6c08cd2209478d7e4be8e826c66a18
AdamBOD <adambarryodonovan@gmail.com>
2021-09-27 22:21:16 +0100

    Added PasswordResetRepository.FindByIdentifierAsync method

--- CMS.Domain.Repositories/User/Interfaces/IPasswordResetRepository.cs
+++ CMS.Domain.Repositories/User/Interfaces/IPasswordResetRepository.cs
@@ -1,8 +1,10 @@
 ﻿using CMS.Domain.Entities;
+using System.Threading.Tasks;
 
 namespace CMS.Domain.Repositories.User.Interfaces
 {
     public interface IPasswordResetRepository : IRepositoryBase<PasswordReset>
     {
+        Task<PasswordReset> FindByIdentifierAsync(string resetIdentifier);
     }
 }
--- CMS.Domain.Repositories/User/PasswordResetRepository.cs
+++ CMS.Domain.Repositories/User/PasswordResetRepository.cs
@@ -1,11 +1,22 @@
 ﻿using CMS.Domain.Entities;
 using CMS.Domain.Repositories.Contexts;
 using CMS.Domain.Repositories.User.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CMS.Domain.Repositories.User
 {
     public class PasswordResetRepository : RepositoryBase<PasswordReset>, IPasswordResetRepository
     {
         public PasswordResetRepository(CMSRepositoryContext context) : base(context) { }
+
+        public async Task<PasswordReset> FindByIdentifierAsync(string resetIdentifier)
+        {
+            var passwordReset = await _CMSContext.PasswordResets.Where(pr =>
+                pr.Identifier == resetIdentifier).FirstOrDefaultAsync();
+
+            return passwordReset;
+        }
     }
 }
