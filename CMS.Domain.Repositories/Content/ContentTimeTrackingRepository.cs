3240dce6e774dbe33983f68d1ccd9d6c3f255825
AdamBOD <adambarryodonovan@gmail.com>
2022-05-02 20:35:56 +0100

    WEB-26 - Updated ContentTimeTrackingRepository (#25)
    * UIpdated Context
    
    * Setup ContentTimeTrackingRepository
    
    * Updated ContentTimeTrackingRepository

--- CMS.Domain.Repositories/Content/ContentTimeTrackingRepository.cs
+++ CMS.Domain.Repositories/Content/ContentTimeTrackingRepository.cs
@@ -1,10 +1,38 @@
 ﻿using CMS.Domain.Entities;
 using CMS.Domain.Repositories.Content.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CMS.Domain.Repositories.Content
 {
     public class ContentTimeTrackingRepository : RepositoryBase<ContentTimeTracking>, IContentTimeTrackingRepository
     {
         public ContentTimeTrackingRepository(CMSContext context) : base(context) { }
+
+        public async Task<ContentTimeTracking> GetByContentIdAsync(Guid contentId)
+        {
+            return await _CMSContext.Set<ContentTimeTracking>()
+                .Where(c => c.ContentId == contentId)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<ContentTimeTracking> GetByUserIdAsync(Guid userId)
+        {
+            return await _CMSContext.Set<ContentTimeTracking>()
+                .Where(c => c.UserId == userId)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<ContentTimeTracking> GetByContentIdAndUserIdAsync(Guid contentId, Guid userId)
+        {
+            return await _CMSContext.Set<ContentTimeTracking>()
+                .Where(c => c.ContentId == contentId && c.UserId == userId)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+        }
     }
 }
--- CMS.Domain.Repositories/Content/Interfaces/IContentTimeTrackingRepository.cs
+++ CMS.Domain.Repositories/Content/Interfaces/IContentTimeTrackingRepository.cs
@@ -1,8 +1,15 @@
 ﻿using CMS.Domain.Entities;
+using System;
+using System.Threading.Tasks;
 
 namespace CMS.Domain.Repositories.Content.Interfaces
 {
     public interface IContentTimeTrackingRepository : IRepositoryBase<ContentTimeTracking>
     {
+        Task<ContentTimeTracking> GetByContentIdAsync(Guid contentId);
+
+        Task<ContentTimeTracking> GetByUserIdAsync(Guid userId);
+
+        Task<ContentTimeTracking> GetByContentIdAndUserIdAsync(Guid contentId, Guid userId);
     }
 }
