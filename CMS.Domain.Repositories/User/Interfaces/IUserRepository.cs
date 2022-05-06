bd6fac4a8fa685d5dc9449463d778d2b1152b820
AdamBOD <adambarryodonovan@gmail.com>
2022-05-06 21:23:30 +0100

    Updated UserRepository

--- CMS.Domain.Repositories/User/Interfaces/IUserRepository.cs
+++ CMS.Domain.Repositories/User/Interfaces/IUserRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace CMS.Domain.Repositories.User.Interfaces
+﻿using CMS.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CMS.Domain.Repositories.User.Interfaces
 {
     public interface IUserRepository : IRepositoryBase<Entities.User>
     {
+        new Task<IEnumerable<VGetUser>> GetPageAsync(int pageNumber, int pageSize);
     }
 }
--- CMS.Domain.Repositories/User/UserRepository.cs
+++ CMS.Domain.Repositories/User/UserRepository.cs
@@ -1,10 +1,25 @@
 ﻿using CMS.Domain.Entities;
 using CMS.Domain.Repositories.User.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CMS.Domain.Repositories.User
 {
     public class UserRepository : RepositoryBase<Entities.User>, IUserRepository
     {
         public UserRepository(CMSContext context) : base(context) { }
+
+        public async new Task<IEnumerable<VGetUser>> GetPageAsync(int pageNumber, int pageSize)
+        {
+            var offset = pageSize * (pageNumber - 1);
+
+            return await _CMSContext.VGetUsers
+                .Skip(offset)
+                .Take(pageSize)
+                .AsNoTracking()
+                .ToListAsync();
+        }
     }
 }
